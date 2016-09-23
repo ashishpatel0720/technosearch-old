8b4a62323d6b4016f0ba6dba028c7d69e20d1780
Manohar Lakkoju <manoharvirat@gmail.com>
2016-09-24 05:27:39 +0530

    Gn

--- angular/app/components/events/events.component.js
+++ angular/app/components/events/events.component.js
@@ -6,6 +6,87 @@ class EventsController{
     }
 
     $onInit(){
+
+        /* Portfolio
+         ---------------------------------------------------------------------- */
+        // $(document).ready(function () {
+        //     (function () {
+        //
+        //         if (!$.fn.isotope) return;
+        //
+        //
+        //         var $container = $('.items'),
+        //             $win = $(window),
+        //             $imgs = $('img.lazy');
+        //
+        //         if ($container.length) {
+        //
+        //
+        //             /* Isotope
+        //              ------------------------- */
+        //             onWindowResize(function () {
+        //                 $container.isotope('layout');
+        //             });
+        //             $(window).load(function () {
+        //                 $container.isotope({
+        //                     itemSelector: '.item',
+        //                     onLayout: function () {
+        //                         $win.trigger('scroll');
+        //                     }
+        //                 });
+        //             });
+        //
+        //             // Load images
+        //             $imgs.jail({effect: 'fadeIn'});
+        //
+        //             // Add filter event
+        //             function _items_filter($el, $data) {
+        //
+        //                 // Add all filter class
+        //                 $el.addClass('item-filter');
+        //
+        //                 // Add categories to item classes
+        //                 $('.item', $container).each(function (i) {
+        //                     var
+        //                         $this = $(this);
+        //                     $this.addClass($this.attr($data));
+        //                 });
+        //
+        //                 $el.on('click', 'a', function (e) {
+        //                     var
+        //                         $this = $(this),
+        //                         $option = $this.attr($data);
+        //
+        //                     // Add active filter class
+        //                     $('.item-filter').removeClass('active-filter');
+        //                     $el.addClass('active-filter');
+        //                     $('.item-filter:not(.active-filter) li a').removeClass('active');
+        //                     $('.item-filter:not(.active-filter) li:first-child a').addClass('active');
+        //
+        //                     // Add/remove active class for this filter
+        //                     $el.find('a').removeClass('active');
+        //                     $this.addClass('active');
+        //
+        //                     if ($option) {
+        //                         if ($option !== '*') $option = $option.replace($option, '.' + $option)
+        //                         $container.isotope({filter: $option});
+        //                     }
+        //
+        //                     e.preventDefault();
+        //
+        //                 });
+        //
+        //                 $el.find('a').first().addClass('active');
+        //             }
+        //
+        //             // Init filters
+        //             if ($('.dd-filter-list').length) _items_filter($('.dd-filter-list'), 'data-categories');
+        //             if ($('.filter-list').length) _items_filter($('.filter-list'), 'data-categories');
+        //
+        //         }
+        //     })();
+        // });
+
     }
 }
 

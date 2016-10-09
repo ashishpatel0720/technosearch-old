255cc2a5ff689ca13e8eeaee73e71f36983a7671
Manohar Lakkoju <manoharvirat@gmail.com>
2016-10-09 14:48:20 +0530

    fsjfdlkdj

---
+++ app/Http/Controllers/TeamController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class TeamController extends Controller
+{
+    //
+    public function getTeam()
+    {
+        return response()->success([
+            ['img_src'=>'x', 'member_name'=>'Hariom Sharma', 'member_title'=>'Web Developer', 'fb_link'=>'http://www.facebook.com'],
+            ['img_src'=>'x', 'member_name'=>'Manohar Lakkoju', 'member_title'=>'Web Developer', 'fb_link'=>'http://www.facebook.com'],
+            ['img_src'=>'x', 'member_name'=>'Kartik Akojwar', 'member_title'=>'Vice President', 'fb_link'=>'http://www.facebook.com'],
+            ['img_src'=>'x', 'member_name'=>'Akshay Gupta', 'member_title'=>'Sports Secretary', 'fb_link'=>'http://www.facebook.com']
+        ]);
+    }
+}
---
+++ database/migrations/2016_10_09_001152_update_event_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateEventTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('event', function (Blueprint $table){
+            $table->boolean('problem_statement')->after('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

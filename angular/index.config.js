3cee1c9809f594df667a700fd66bf2a6456b1d3b
Hariom Sharma <hariom0sharma@gmail.com>
2016-10-11 02:40:26 +0530

    home page completed

--- angular/index.config.js
+++ angular/index.config.js
@@ -1,8 +1,6 @@
-import {ToastrConfigConfig} from './config/ToastrConfig.config';
 import {RoutesConfig} from './config/routes.config';
 import {LoadingBarConfig} from './config/loading_bar.config';
 
 angular.module('app.config')
-	.config(ToastrConfigConfig)
     .config(RoutesConfig)
 	.config(LoadingBarConfig)

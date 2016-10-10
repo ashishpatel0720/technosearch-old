35e11bb31cb5dbd5aa6745caecf881f4ff560baa
Hariom Sharma <hariom0sharma@gmail.com>
2016-10-11 01:31:28 +0530

    home page completed

--- angular/run/routes.run.js
+++ angular/run/routes.run.js
@@ -1,3 +1,7 @@
-export function RoutesRun($state, $transitions) {
+export function RoutesRun($transitions,$location,$window) {
     'ngInject';
+    $window.ga('create', 'UA-51356287-2', 'auto');
+    $transitions.onSuccess({from: '*.*', to: '*.*'}, function() {
+        $window.ga('send', 'pageview', $location.path());
+    });
 }
